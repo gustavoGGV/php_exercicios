4ae2ec2e3b7f66e08993366a383227b8af316187
Gustavo <134728517+gustavoGGV@users.noreply.github.com>
2024-02-08 15:02:22 -0300

    Create contar_1_a_10.php

---
+++ contar_1_a_10.php
@@ -0,0 +1,23 @@
+<?php
+
+print "while\n";
+
+$n=0;
+while($n<10){
+    $n++;
+    print $n . "\n";
+}
+
+print "\ndo-while\n";
+
+$n=0;
+do{
+    $n++;
+    print $n . "\n";
+} while($n<10);
+
+print "\nfor\n";
+
+for($i=1;$i<11;$i++){
+    print $i . "\n";
+}
